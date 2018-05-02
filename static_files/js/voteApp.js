9d1d1c1f5a085bba1765d180e1c740e90b683ecc
German Flores <gfloresa@ucsd.edu>
2018-05-02 10:41:20 -0700

    ajax and api use

---
+++ static_files/js/voteApp.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Javascript connected!");
+
+	//const url = "https://api.nytimes.com/svc/search/v2/candidatesearch.json?api-key=bebd5673446e40c0a0613cd3ea327a8d";
+	const $candidates = $('#candidates');
+
+	$('#refreshFeed').click(function(e) {
+		e.preventDefault();
+		console.log('button clicked');
+		$.ajax({
+			type: 'GET',
+			url: 'https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyBrvfLVDyTJ1Ar8EdM1aZda_9141qQcCh4&address=1263%20Pacific%20Ave.%20Kansas%20City%20KS&electionId=2000',
+			dataType: 'json',
+			success: function(candidates) {
+					console.log("google api loaded");
+          $.each(candidates.contests, function(i, candidate){
+            $candidates.append(`<li>Representatives: <b>${candidates.contests[i].candidates[0].name}</b></li>`);
+            console.log(candidates.contests[i].candidates[0].name);
+				  });
+			}
+		});
+
+  	console.log('test');
+  });
+}
